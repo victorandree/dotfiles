800fa8e65cd0600d293b576d37a83a5a682934c2
Victor Andrée <victor@victorandree.se>
2017-11-08 18:59:34 +0100

    refactor: find fzf in /usr/share

--- shell/.config/shell/zsh/fzf.zsh
+++ shell/.config/shell/zsh/fzf.zsh
@@ -1,20 +1,3 @@
-# Setup fzf
-# ---------
-if [[ ! "$PATH" == */usr/local/opt/fzf/bin* ]]; then
-  export PATH="$PATH:/usr/local/opt/fzf/bin"
-fi
-
-# Man path
-# --------
-if [[ ! "$MANPATH" == */usr/local/opt/fzf/man* && -d "/usr/local/opt/fzf/man" ]]; then
-  export MANPATH="$MANPATH:/usr/local/opt/fzf/man"
-fi
-
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.zsh" 2> /dev/null
-
-# Key bindings
-# ------------
-FILE="/usr/local/opt/fzf/shell/key-bindings.zsh" ; [ -f $FILE ] && source $FILE
+[[ $- == *i* ]] && source "/usr/share/fzf/completion.zsh" 2> /dev/null
+FILE="/usr/share/fzf/key-bindings.zsh" ; [ -f $FILE ] && source $FILE
 
