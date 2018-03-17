c12a22503d553aee1f68cc8327a2dab383a17aa0
Victor Andrée <victor.andree@advinans.se>
2018-03-17 11:22:51 +0100

    feat: hammerspoon uuid spitter

---
+++ hammerspoon/.hammerspoon/init.lua
@@ -0,0 +1,14 @@
+hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "R", function()
+  hs.reload()
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
+  insert(hs.host.uuid())
+end)
+
+function insert (x)
+  local pasteboardContents = hs.pasteboard.getContents()
+  hs.pasteboard.setContents(x)
+  hs.eventtap.keyStroke({"cmd"}, "v")
+  hs.pasteboard.setContents(pasteboardContents)
+end
