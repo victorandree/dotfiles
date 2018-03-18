4a4686fe800797104cb40e8a7c5e16804421fb01
Victor Andrée <victor.andree@advinans.se>
2018-03-18 20:48:43 +0100

    feat: generate ids with hammerspoon

--- hammerspoon/.hammerspoon/common.lua
+++ hammerspoon/.hammerspoon/common.lua
@@ -1,12 +1,20 @@
 -- Create "init.lua" in Hammerspoon and include this file to get started
 -- I.e. `require "common"`
 
+function trim(s)
+  return (s:gsub("^%s*(.-)%s*$", "%1"))
+end
+
 hs.hotkey.bind({"cmd", "alt", "ctrl", "shift"}, "R", function()
   hs.reload()
 end)
 
 hs.hotkey.bind({"cmd", "alt", "ctrl"}, "U", function()
   insert(string.lower(hs.host.uuid()))
+end)
+
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "I", function()
+  insert(string.sub(string.lower(hs.host.uuid()), 0, 8))
 end)
 
 function insert (x)
