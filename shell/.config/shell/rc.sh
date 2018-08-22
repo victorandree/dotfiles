bc074d05c1bfc65b266379ddcbb25b5486c3d7fa
Victor Andrée <victor.andree@advinans.se>
2018-08-22 14:26:35 +0200

    fix(shell): dont use base16

--- shell/.config/shell/rc.sh
+++ shell/.config/shell/rc.sh
@@ -64,8 +64,9 @@ fi
 # True color support for Neovim
 export NVIM_TUI_ENABLE_TRUE_COLOR=1
 
-BASE16_SHELL=$HOME/.config/shell/base16-shell
-[ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
+# Use Base16 shell colors
+# BASE16_SHELL=$HOME/.config/shell/base16-shell
+# [ -n "$PS1" ] && [ -s $BASE16_SHELL/profile_helper.sh ] && eval "$($BASE16_SHELL/profile_helper.sh)"
 
 # This takes just a little bit too much time!! Use modern techniques
 # instead.
