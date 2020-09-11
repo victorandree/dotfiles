2a51c4490214ef9ff665d8abd45cb6fbd4f9e258
Victor Andrée <hello@victorandree.se>
2020-09-11 11:06:46 +0200

    feat: add nord dircolors for macos

--- shell/.config/login_macos.sh
+++ shell/.config/login_macos.sh
@@ -9,6 +9,15 @@ export LANG="en_US.UTF-8"
 # Enable cursor switching in nvim
 export NVIM_TUI_ENABLE_CURSOR_SHAPE="1"
 
+# If GNU ls is available, use it and use colorized output
+if command -v gls >/dev/null; then
+  alias ls="gls -l --color"
+fi
+
+if command -v gdircolors >/dev/null && test -r "$XDG_CONFIG_HOME/dircolors"; then
+  eval $(gdircolors "$XDG_CONFIG_HOME/dircolors")
+fi
+
 # This is for quick Clojure start-up
 export BOOT_JVM_OPTIONS="-client
   -XX:+TieredCompilation
